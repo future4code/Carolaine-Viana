23df56cae8bbffa8e3a01295d0f51b15bb05b234
Carolaine Viana <carolaine.viana@MacBook-Pro-de-Carolaine.local>
2020-12-11 12:08:37 -0300

    parte 01 feita

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,34 @@
+// Faça um commit a cada exercício concluído.
+
+
+// ***** # Lista de Exercícios: ***** ///
+
+// 1)
+// conversao do dolar para reais. nesse caso sao 100 reais, convertendo para dolar.
+
+// 2)
+// nesse exemplo, está demonstrando a taxa de rendimento de investimentos variados. como, poupanca, renda fixa etc..
+//entao depdendo do valor investido, e do investimento escolhido, ira render x. no dofault diz se nenhuma opcao valida por escolhida
+// aquela mensagem ira aparecer: tipo de investimento informado incorreto.
+// os proximos const, sao a escolha que o usuario fez de investir, naquele caso do novomontante, 150 reais em acoes.
+// e depois segundo montante, 200 reais em tesouro direto.
+//apos isso ira imprimir.
+
+//3)
+// se o array de numero conter elemento pares, o array1 que esta vazio vai receber esses valores (pares) encontrados.
+// se nao, o arra2 que tbm esta vazio, contera os arrays impares.
+
+//4
+// se o numero do array de numero for MENOR que o numero inifinito dentro da variavel number1
+// essa variavel de num infinito vai receber esse numero agora. 
+
+
+//se o numero do array de numero for maior que a variavel numero que contem 0, essa variavel que contem 0 agora vai receber esse numero.
+
+
+//no console log sera impresso os numeros da variavel num1 e num2.
+
+
+
+
+
