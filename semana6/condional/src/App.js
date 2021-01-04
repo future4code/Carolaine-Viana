dc1b8db50fc6fad90dd2c3584486985ea317e593
Carolaine Viana <carolaine.viana@MacBook-Pro-de-Carolaine.local>
2021-01-04 18:49:29 -0300

    vendo se foi feito o push

--- semana6/condional/src/App.js
+++ semana6/condional/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import React from 'react';
-import Etapa1 from './components/Etapa1';
-import Etapa2 from './components/Etapa2';
-import Etapa3 from './components/Etapa3';
-import Final from './components/Final';
+import './App.css'
+import React from 'react'
+import Etapa1 from './components/Etapa1'
+import Etapa2 from './components/Etapa2'
+import Etapa3 from './components/Etapa3'
+import Final from './components/Final'
 import styled from 'styled-components'
 
 const BotaoProximo = styled.button`
